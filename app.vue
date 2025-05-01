75cce55450d7cb8296fefa3613a120e3cf7edb02
ian Kwon <sunghwan.ian.kwon@gmail.com>
2025-05-01 17:13:13 +0900

    chore: 카카오 map 관련 api src 추가가

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup>
+const config = useRuntimeConfig()
+useHead({
+  script: [
+    {
+      src: `//dapi.kakao.com/v2/maps/sdk.js?autoload=true&appkey=${config.public.kakaoMapKey}&libraries=services`,
+    },
+  ],
+})
+</script>
+
 <template>
   <v-app>
     <NuxtLayout />
