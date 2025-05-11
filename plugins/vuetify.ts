f0a1ed867daa84396d015ff5fb0b61a9227af2ab
ian Kwon <sunghwan.ian.kwon@gmail.com>
2025-05-11 14:40:55 +0900

    chore: vuetify 한글 기본 언어로 설정정

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,9 +1,14 @@
 import { createVuetify } from 'vuetify'
+import { ko } from 'vuetify/locale'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
+    locale: {
+      locale: 'ko',        // 기본 언어를 한글로 설정
+      messages: { ko },    // 한글 메시지 로딩
+    },
     components,
     directives,
   })
